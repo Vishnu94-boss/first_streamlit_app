bb736c6685feb0f9f77d69e82e125b443f92e71d
Vishnu <121834191+Vishnu94-boss@users.noreply.github.com>
2023-01-02 19:05:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,6 @@ streamlit.text('🥚Hard-bolied eggs')
 streamlit.text('🥑Avacado Toast')
 
 import pandas
-
-
 my_fruit_list = pandas.readcsv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
